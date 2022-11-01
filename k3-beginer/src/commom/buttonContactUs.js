a6bd60429f2f228fc133b8cc8e3584aa6af2de1d
VanBac65 <vanbac6500@gmail.com>
2022-11-01 09:46:55 +0700

    btvn-func

---
+++ k3-beginer/src/commom/buttonContactUs.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../style/commom/button.css'
+
+export default function ButtonContactUs() {
+  return (
+    <div className='btn-contact'>
+      <button>CONTACT US</button>
+    </div>
+  )
+}
