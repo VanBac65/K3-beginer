beaf2bf11e3cce4cd58641791731473b9dd8ef8a
VanBac65 <vanbac6500@gmail.com>
2022-11-01 14:47:55 +0700

    class component

--- k3-beginer/src/commom/buttonContactUs.js
+++ k3-beginer/src/commom/buttonContactUs.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../style/commom/button.css'
 
-export default function ButtonContactUs() {
-  return (
-    <div className='btn-contact'>
-      <button>CONTACT US</button>
-    </div>
-  )
+export default class buttonContactUs extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'CONTACT US', check: true
+    }
+    this.btnClick = this.btnClick.bind(this)
+  }
+
+  btnClick() {
+    this.setState({ name: 'DONE!!', check: false })
+  }
+
+
+
+  componentDidUpdate() {
+    alert('DONE UP DATE!!')
+  }
+
+  componentDidMount() {
+    // alert('DONE DID MOUNT!!')
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then(response => response.json())
+    //   .then(json => console.log('json',json))
+    console.log('Component DidMount')
+  }
+
+  render() {
+    console.log('abc')
+    console.log(this.state.check)
+
+    let dom
+
+    if (this.state.check) {
+      dom = <ButtonContactUs2 />
+    }
+
+    return (
+      
+      <div className='btn-contact'>
+        {console.log(123)}
+        {dom}
+        <button onClick={this.btnClick}>{this.state.name}</button>
+      </div>
+
+    )
+  }
 }
+
+export class ButtonContactUs2 extends Component {
+  componentWillUnmount() {
+    alert('DONE UNMOUNT!!')
+  }
+
+  render() {
+    return (
+      <div>buttonContactUs</div>
+    )
+  }
+}
+
+
--- k3-beginer/src/commom/titleDes.js
+++ k3-beginer/src/commom/titleDes.js
@@ -1,15 +1,41 @@
-import React from 'react'
+// import React from 'react'
 import '../style/commom/titleDes.css'
 
-export default function TitleDes({ title, des }) {
-    return (
-        <>
-            <div className='title'>
-                {title}
-            </div>
-            <div className='des'>
-                {des}
-            </div>
-        </>
-    )
+// export default function TitleDes({ title, des }) {
+//     return (
+//         <>
+//             <div className='title'>
+//                 {title}
+//             </div>
+//             <div className='des'>
+//                 {des}
+//             </div>
+//         </>
+//     )
+// }
+
+import React, { Component } from 'react'
+
+export default class TitleDes extends Component {
+    constructor({ title, des }) {
+        super()
+        this.state = ({
+            title: title,
+            des: des
+        })
+
+    }
+    render() {
+        return (
+            <>
+                <div className='title'>
+                    {this.state.title}
+                </div>
+                <div className='des'>
+                    {this.state.des}
+                </div>
+            </>
+        )
+    }
 }
+
